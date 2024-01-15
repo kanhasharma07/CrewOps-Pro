19056c8f02d9fe947045923fd72fdbffc577f4ba
Kanha Sharma <kanhasharma07@gmail.com>
2024-01-15 18:41:09 +0530

    Add flight crew management feature with HTML templates and Flask routing

---
+++ app.py
@@ -0,0 +1,56 @@
+from flight_crew import FlightCrew
+
+from flask import Flask, url_for, redirect, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/addCrew', methods = ['GET', 'POST'])
+def addCrew():
+    
+    if request.method == 'GET':
+        return render_template('addCrew.html')
+    else:
+        receivedFlightCrewData =[
+            request.form['sap'],
+            request.form['fname'],
+            request.form['lname'],
+            request.form['desig'],
+            request.form['mob'],
+            request.form['atpl'],
+            request.form['license'],
+            request.form['medical'],
+            request.form['baseops'],
+            True,
+            request.form['pw']
+        ]
+    
+        
+        FlightCrew.addCrew(receivedFlightCrewData[0],
+                           receivedFlightCrewData[1],
+                           receivedFlightCrewData[2],
+                           receivedFlightCrewData[3],
+                           receivedFlightCrewData[4],
+                           receivedFlightCrewData[5],
+                           receivedFlightCrewData[6],
+                           receivedFlightCrewData[7],
+                           receivedFlightCrewData[8],
+                           receivedFlightCrewData[9],
+                           receivedFlightCrewData[10],
+                           )
+        
+        
+        print(receivedFlightCrewData)
+        return render_template('addflightcrewsuccessful.html')
+    
+    
+@app.route('/test')
+def test():
+    return render_template('addflightcrewsuccessful.html')
+
+if __name__=='__main__':
+    app.run(port=8000, debug=True)
--- flight_crew.py
+++ flight_crew.py
@@ -6,18 +6,7 @@ from models.flight_crew_model import FlightCrewModel
 class FlightCrew:
     
     @staticmethod
-    def addCrew(sap: int,
-                fname: str,
-                lname: str,
-                desig: str,
-                mob: int,
-                atpl: bool,
-                license: int,
-                medical: date,
-                baseops: str,
-                avail: bool,
-                pw: str,
-                login = None):
+    def addCrew(sap, fname, lname, desig, mob, atpl, license, medical, baseops, avail, pw, login = None):
         # Creating operational Flight Crew Instance
         pilot = FlightCrewModel(
             sap=sap,
