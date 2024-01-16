d9c6e5f3275db9e52386dd68276aba2d61787390
Kanha Sharma <kanhasharma07@gmail.com>
2024-01-17 02:05:15 +0530

    Implement delete crew functionality and improve code formatting

--- app.py
+++ app.py
@@ -4,56 +4,66 @@ from flask import Flask, url_for, redirect, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
-def home_page():
-    return render_template('home.html')
 
-@app.route('/addCrew', methods = ['GET', 'POST'])
+@app.route("/")
+@app.route("/home")
+def home_page():
+    return render_template("home.html")
+
+
+@app.route("/addCrew", methods=["GET", "POST"])
 def addCrew():
-    
-    if request.method == 'GET':
-        return render_template('addCrew.html')
+    if request.method == "GET":
+        return render_template("addCrew.html")
     else:
-        receivedFlightCrewData =[
-            request.form['sap'],
-            request.form['fname'],
-            request.form['lname'],
-            request.form['desig'],
-            request.form['mob'],
-            request.form['atpl'],
-            request.form['license'],
-            request.form['medical'],
-            request.form['baseops'],
+        receivedFlightCrewData = [
+            request.form["sap"],
+            request.form["fname"],
+            request.form["lname"],
+            request.form["desig"],
+            request.form["mob"],
+            request.form["atpl"],
+            request.form["license"],
+            request.form["medical"],
+            request.form["baseops"],
             True,
-            request.form['pw']
+            request.form["pw"],
         ]
-    
-        FlightCrew.addCrew(receivedFlightCrewData[0],
-                           receivedFlightCrewData[1],
-                           receivedFlightCrewData[2],
-                           receivedFlightCrewData[3],
-                           receivedFlightCrewData[4],
-                           receivedFlightCrewData[5],
-                           receivedFlightCrewData[6],
-                           receivedFlightCrewData[7],
-                           receivedFlightCrewData[8],
-                           receivedFlightCrewData[9],
-                           receivedFlightCrewData[10],
-                           )
-        
-        
-        print(receivedFlightCrewData)
-        return render_template('addflightcrewsuccessful.html')
-    
-@app.route('/viewCrew')
+
+        FlightCrew.addCrew(
+            receivedFlightCrewData[0],
+            receivedFlightCrewData[1],
+            receivedFlightCrewData[2],
+            receivedFlightCrewData[3],
+            receivedFlightCrewData[4],
+            receivedFlightCrewData[5],
+            receivedFlightCrewData[6],
+            receivedFlightCrewData[7],
+            receivedFlightCrewData[8],
+            receivedFlightCrewData[9],
+            receivedFlightCrewData[10],
+        )
+        return render_template("addflightcrewsuccessful.html")
+
+
+@app.route("/viewCrew")
 def viewCrew():
     crewView = FlightCrew.viewCrew()
-    return render_template('viewCrew.html', crewView=crewView)
+    return render_template("viewCrew.html", crewView=crewView)
+
+
+@app.route("/deleteCrew", methods=["GET", "POST"])
+def deleteCrew():
+    if request.method == "GET":
+        return render_template("deleteCrew.html")
+    else:
+        FlightCrew.deleteCrew(sap=request.form["sap"])
+        return render_template("deleteCrewSuccessful.html", sap=request.form["sap"])
+
 
 # @app.route('/test')
 # def test():
-#     return render_template('addflightcrewsuccessful.html')
+#     return render_template('deleteCrew.html')
 
-if __name__=='__main__':
+if __name__ == "__main__":
     app.run(port=8000, debug=True)
--- flight_crew.py
+++ flight_crew.py
@@ -5,7 +5,20 @@ from models.flight_crew_model import FlightCrewModel
 
 class FlightCrew:
     @staticmethod
-    def addCrew(sap, fname, lname, desig, mob, atpl, license, medical, baseops, avail, pw, login = None):
+    def addCrew(
+        sap,
+        fname,
+        lname,
+        desig,
+        mob,
+        atpl,
+        license,
+        medical,
+        baseops,
+        avail,
+        pw,
+        login=None,
+    ):
         # Creating operational Flight Crew Instance
         pilot = FlightCrewModel(
             sap=sap,
@@ -19,46 +32,40 @@ class FlightCrew:
             base_ops=baseops,
             availability=avail,
             login=login,
-            pw=pw)
-        
-        
-        table_name = 'flight_crew'
+            pw=pw,
+        )
+
+        table_name = "flight_crew"
         query = f"""INSERT INTO {table_name}
         (staffid, fname, lname, designation, contact, atpl, license_no, medical_validity, base_ops, availability, login, pw)
         VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-        data= tuple(pilot.model_dump().values())
+        data = tuple(pilot.model_dump().values())
         db.execute(query, data)
         connection.commit()
-    
-    @staticmethod    
+
+    @staticmethod
     def deleteCrew(sap):
         db.execute(f"DELETE FROM flight_crew WHERE staffid = {sap}")
         connection.commit()
-        
+
     @staticmethod
     def viewCrew():
-        table_name = 'flight_crew'
-        query = f'SELECT * FROM {table_name}'
+        table_name = "flight_crew"
+        query = f"SELECT * FROM {table_name}"
         db.execute(query)
         crewViewList = db.fetchall()
-        
+
         def replaceNonBoolean(seq):
             modifiedList = [list(item) for item in seq]
             for i, val in enumerate(modifiedList):
-                modifiedList[i][5] = False if val[5]==0 else True
-                modifiedList[i][9] = False if val[9]==0 else True
+                modifiedList[i][5] = False if val[5] == 0 else True
+                modifiedList[i][9] = False if val[9] == 0 else True
             return modifiedList
-    
+
         return replaceNonBoolean(crewViewList)
-        
-    
-        
-        
-        
-  
-  
-if __name__=='__main__:': 
-    
+
+
+if __name__ == "__main__:":
     FlightCrew.viewCrew()
     # from datetime import date
     # kanha = FlightCrew()
